42ee87d4d0f6f5e5c609f4713334c02134e52b99
Koushik Dey <kousickdey3@gmail.com>
2024-03-24 02:12:19 +0530

    RMX2020: vendorsetup: Update Cloning Script
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,24 @@
-# Kernel for RMX2020
+echo 'Start Cloning repos for RMX2020'
+
+echo 'Cloning Kernel Source [1/4]'
+# Kernel Source
 rm -rf kernel/realme/RMX2020
 git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_realme_RMX2020 -b Rui2-OSS kernel/realme/RMX2020
 
-# Vendor Blobs for RMX2020
+echo 'Cloning Vendor Trees [2/4]'
+# Vendor Blobs
 rm -rf vendor/realme/RMX2020
 git clone https://github.com/ProjectElixir-Devices/vendor_realme_RMX2020 -b UpsideDownCake vendor/realme/RMX2020
+
 rm -rf vendor/realme/RMX2020-ims
 git clone https://github.com/ProjectElixir-Devices/vendor_realme_RMX2020-ims -b UpsideDownCake vendor/realme/RMX2020-ims
 
-# Hardware OSS parts for RMX2020
-rm -rf hardware/mediatek
-git clone https://github.com/ArrowOS/android_hardware_mediatek -b arrow-14.0 hardware/mediatek
+echo 'Cloning Hardware Parts [3/4]'
+# Hardware OSS parts
+rm -rf hardware/oplus
+git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-21 hardware/oplus
 
+echo 'Cloning Mediatek Sepolicy_Vndr [4/4]'
 # Mediatek Sepolicy_vndr
 rm -rf device/mediatek/sepolicy_vndr
-git clone https://github.com/ArrowOS/android_device_mediatek_sepolicy_vndr -b arrow-14.0 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
